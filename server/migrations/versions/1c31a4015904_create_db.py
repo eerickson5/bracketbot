41938122897a7d5946f8a87181c1982a71b5ffb5
Elise Erickson <elisece717@gmail.com>
2024-04-08 21:14:32 -0400

    creates basic  models for all 5 classes

---
+++ server/migrations/versions/1c31a4015904_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: 1c31a4015904
+Revises: 
+Create Date: 2024-04-08 20:36:05.139429
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1c31a4015904'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- server/models.py
+++ server/models.py
@@ -3,4 +3,42 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
-# Models go here!
+class Team(db.Model, SerializerMixin):
+    __table_name__ = "teams"
+    id = db.Column(db.Integer, primary_key = True)
+    team_name = db.Column(db.String)
+    # captain_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+class GameScore(db.Model, SerializerMixin):
+    __table_name__ = "gamescores"
+    id = db.Column(db.Integer, primary_key=True)
+    own_score = db.Column(db.Integer)
+    opponent_score = db.Column(db.Integer)
+    # team_id = db.Column(db.Integer, db.ForeignKey('teams.id'))
+    # game_id = db.Column(db.Integer, db.ForeignKey('games.id'))
+
+class Game(db.Model, SerializerMixin):
+    __table_name__ = "games"
+    id = db.Column(db.Integer, primary_key=True)
+    location = db.Column(db.String)
+    start_time = db.Column(db.DateTime)
+    # stage_id = db.Column(db.Integer, db.ForeignKey('stages.id'))
+    # parent_id = db.Column(db.Integer, db.ForeignKey('games.id'))
+
+class Stage(db.Model, SerializerMixin):
+    __table_name__ = "stages"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    start_time = db.Column(db.DateTime)
+    # end_time = db.Column(db.DateTime)
+    minutes_per_game = db.Column(db.Integer)
+    minutes_per_break = db.Column(db.Integer)
+    is_bracket = db.Column(db.Boolean)
+    # tournament_id = db.Column(db.Integer, db.ForeignKey('tournaments.id'))
+
+
+class Tournament(db.Model, SerializerMixin):
+    __table_name__ = "tournaments"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    num_fields = db.Column(db.Integer)
