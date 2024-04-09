01e18a13cda17a53153a3f561ee3fc74fc99ba73
Elise Erickson <elisece717@gmail.com>
2024-04-09 13:50:34 -0400

    adds routing to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import App from "./components/App";
+import * as ReactDOM from "react-dom";
 import "./index.css";
 import { createRoot } from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>
+    }
+])
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<RouterProvider router={router}/>)
