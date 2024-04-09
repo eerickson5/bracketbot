061641179d58b83f24256d49845b928f6d8c56c5
Elise Erickson <elisece717@gmail.com>
2024-04-09 19:55:51 -0400

    adds GameCard

---
+++ client/src/components/GameCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Card } from 'semantic-ui-react'
+
+export default function GameCard({game}){
+    return (
+        <Card
+        header= {`${game.game_scores[0].team.name} vs ${game.game_scores[1].team.name}`}
+        description= {game.stage.name + " | " + game.location}
+        meta= {`${game.game_scores[0].team.name} ${game.game_scores[0].own_score} - ${game.game_scores[1].own_score} ${game.game_scores[1].team.name}`}
+        />
+    )
+}
--- client/src/pages/TournamentDashboard.js
+++ client/src/pages/TournamentDashboard.js
@@ -1,22 +1,47 @@
 import React, { useEffect, useState } from "react";
 import {
-    Card, CardContent, CardHeader, CardMeta, CardDescription, Container
+    Container
   } from 'semantic-ui-react'
 
 import TournamentCard from "../components/TournamentCard";
+import GameCard from "../components/GameCard";
 
 export default function TournamentDashboard(){
-  const tourney = {image:'https://universe.byu.edu/wp-content/uploads/2019/07/AP19204604544330-1.jpg',
-  name:'Round Robin',
-  location:'Applewood Park',
-  numTeams: 16,
-  numPools: 4,
-  numBrackets: 2}
+  const tourney = {
+    image:'https://universe.byu.edu/wp-content/uploads/2019/07/AP19204604544330-1.jpg',
+    name:'Round Robin',
+    location:'Applewood Park',
+    numTeams: 16,
+    numPools: 4,
+    numBrackets: 2
+  }
 
-  
+  const game = {
+    location: 'Field C',
+    game_scores: [
+      {
+        team: {
+          name: "Magma"
+        },
+        own_score: 15
+      },
+      {
+        team: {
+          name: "Ozone"
+        },
+        own_score: 13
+      }
+    ],
+    stage: {
+      name: "Pool B"
+    }
+  }
+
+
     return(
     <Container>
       <TournamentCard tournament={tourney}/>
+      <GameCard game={game}/>
     </Container>
      
     )
