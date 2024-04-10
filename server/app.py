7a6c4da80205d9b65f31c553e4a400ae0619a49c
Elise Erickson <elisece717@gmail.com>
2024-04-10 13:02:46 -0400

    adds first api destination

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, make_response
 from flask_restful import Resource
 
 # Local imports
@@ -17,6 +17,15 @@ from models import Team, Tournament, Game, GameScore, Stage
 def index():
     return '<h1>Project Server</h1>'
 
+class TeamByID(Resource):
+    def get(self, id):
+        team = Team.query.filter(Team.id == id).first()
+        if team:
+            return make_response(team.to_dict(), 200)
+        else:
+            return make_response("No team exists with that id", 404)
+
+api.add_resource(TeamByID, '/team/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
