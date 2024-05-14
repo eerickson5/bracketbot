3994f8018f8ec8c8904ed5e14c5bba5303bfbb50
Elise Erickson <elisece717@gmail.com>
2024-05-14 12:38:55 -0400

    adds ui for tournament delete

--- client/src/dashboards/Settings.js
+++ client/src/dashboards/Settings.js
@@ -1,5 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
+import { Button, Input } from 'semantic-ui-react'
 
 export default function Settings(){
-    return( <h1>hi</h1>)
+    const [wantToDelete, setWantToDelete] = useState(false)
+
+    return(
+        <div style={{display: 'flex', flexDirection: 'column', alignContent: 'flex-start'}}>
+            {
+                wantToDelete 
+                ?   <>
+                    <Button
+                        color='blue'
+                        content='Go Back'
+                        icon='cancel'
+                        onClick={() => setWantToDelete(false)}
+                        style={{ alignSelf: 'flex-start', marginBottom: 10}}
+                    />
+                    <Input 
+                        placeholder='Type tournament name to permanently delete it...' 
+                        action={{
+                            color: 'red',
+                            labelPosition: 'right',
+                            icon: 'trash',
+                            content: 'Delete',
+                        }}/>
+                    </>
+                :   <Button
+                    color='red'
+                    content='Delete Tournament'
+                    icon='trash'
+                    label={{ basic: true, color: 'red', pointing: 'left', content: 'This will delete all associated games.' }}
+                    onClick={() => setWantToDelete(true)}
+                />
+            }
+            
+            
+        </div>
+        
+    )
 }
