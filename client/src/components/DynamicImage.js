5cec4444946bef726b998a8038439058ea0134da
Elise Erickson <elisece717@gmail.com>
2024-05-16 13:47:53 -0400

    adjusts tournament card for emojis

--- client/src/components/DynamicImage.js
+++ client/src/components/DynamicImage.js
@@ -2,17 +2,16 @@ import React from "react";
 import { Image, Header } from 'semantic-ui-react'
 import { isSingleEmoji } from "../emojiFunctions";
 
-export default function DynamicImage({image, isFullWidth}){
-    const emojis = Array.from({ length: 15 }, (_, index) => index + 1);
-
+export default function DynamicImage({image, isFullWidth, maxWidth='100%', maxHeight='200px'}){
+    const emojis = Array.from({ length: isFullWidth ? 15 : 20 }, (_, index) => index + 1);
     const randomPosition = (max) => Math.floor(Math.random() * max);
     const randomSize = () => Math.floor(Math.random() * 70) + 20;
     const randomRotation = () => Math.floor(Math.random() * 360);
     const randomOpacity = () => Math.random() * 0.2 + 0.6;
 
     return isSingleEmoji(image) 
-    ? <Header size='huge'>
-        <div style={{ position: 'relative', width: '100%', height: '200px' }}>
+    ? 
+        <div style={{ position: 'relative', overflow:'hidden', width: maxWidth, height: maxHeight, backgroundColor: '#e8e8e8' }}>
             {emojis.map((_, index) => (
                 <span
                 key={index}
@@ -28,8 +27,7 @@ export default function DynamicImage({image, isFullWidth}){
                 {image}
                 </span>
             ))}
-            </div>
-        </Header> 
+        </div>
     : isFullWidth 
     ? <img src={image} alt="Tournament Logo" style={{maxHeight: 250, width:"100%", objectFit:"cover"}} />
     : <Image src={image} alt="Tournament Logo" fluid rounded style={{maxWidth: 500}}/>
--- client/src/components/TournamentCard.js
+++ client/src/components/TournamentCard.js
@@ -1,15 +1,21 @@
 import React from "react";
-import { Card } from 'semantic-ui-react'
+import { Card, Divider, Header, Segment } from 'semantic-ui-react'
+import { isSingleEmoji } from "../emojiFunctions";
+import DynamicImage from "./DynamicImage";
 
 export default function TournamentCard({tournament}){
-    
     return (
         <Card
-        image={tournament.image}
+        image={<DynamicImage 
+                image={tournament.image} 
+                isFullWidth={false}
+                maxWidth="100%"
+                maxHeight="80%"
+                />}
         header={tournament.name}
         // meta={tournament.location}
-        // description={`${tournament.teams.length} teams | ${tournament.numPools ?? 0} Pools | ${tournament.numBrackets ?? 0} Brackets`}
-        color="red"
+        description={`${tournament.teams.length} teams`}
+        style={{width: '30%', minHeight: 200}}
         />
-    )
+        )
 }
