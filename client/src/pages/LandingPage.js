a5ae4ae2aea33f5eede4e473361db2de12ec4006
Elise Erickson <elisece717@gmail.com>
2024-05-18 16:41:41 -0400

    adds image to landing page

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,15 +1,21 @@
 import React from "react";
 import LoginForm from "../dashboards/LoginForm";
-import { Container } from "semantic-ui-react";
+import { Image } from "semantic-ui-react";
 
 export default function LandingPage(){
     return(
         <div>
-            <div style={{display: 'flex', flexDirection:'row'}}>
+            <div>
+                <Image src='https://i.imgur.com/56YwqBA.png' size="tiny"/>
+            </div>
+            
+            <div style={{display: 'flex', flexDirection:'row', padding: 20, alignItems: 'center', justifyContent: 'space-evenly'}}>
                 <div>
                     <h2>Build tournament schedules from the first round ➜ finals</h2>
                     <h4>Add teams, plan pools, schedule games, and build brackets in seconds</h4>
                 </div>
+                <Image src='https://i.imgur.com/C8qi8ro.png' fluid rounded bordered size="large"/>
+                
             </div>
             
             
